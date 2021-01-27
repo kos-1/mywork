fff13f6d4805e203ca5ef20da511778493eb5f38
kos-1 <77705189+kos-1@users.noreply.github.com>
2021-01-26 20:41:56 -0500

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AmplifySignOut />
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello From V2!!!</h1>
+        <h1>Welcome Back</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
